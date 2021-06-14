ef4bd52f6a884b85824b9b7661536832b32e2565
Olexandr Dolbin <olexandr.dolbin@gmail.com>
2021-06-14 06:05:24 +0300

    small changes

--- src/config.js
+++ src/config.js
@@ -3,7 +3,14 @@ import React from "react";
 const config = {
   SERVER_URL: process.env.SERVER_URL || 'ws://localhost:9000',
   GOOGLE_ANALYTICS_CODE: process.env.REACT_APP_GOOGLE_ANALYTICS_CODE,
-  SENTRY_URI: process.env.REACT_APP_SENTRY_URI
+  SENTRY_URI: process.env.REACT_APP_SENTRY_URI,
+  ROMS: {
+    owlia: {
+      name: "Connect to server",
+      description: "",
+      url: ""
+    },
+  },
 };
 
 export default config;
